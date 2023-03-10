bc1d2d4811b8e088d046001c79284397da63379b
Nitesh000 <ntudu040@gmail.com>
2023-03-10 11:13:28 +0530

    fix(cors): fix a cors error

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ const app = express();
 // importring routes
 import anime from "./routes/anime.js";
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
