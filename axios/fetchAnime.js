210a462b1f54d76f138e3baad126afe41aafd09c
Nitesh000 <ntudu040@gmail.com>
2023-03-10 11:50:48 +0530

    fix: --

--- axios/fetchAnime.js
+++ axios/fetchAnime.js
@@ -2,6 +2,9 @@ import axios from "axios";
 
 const fetchAnime = axios.create({
   baseURL: "https://api.consumet.org/anime/gogoanime",
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+  },
 });
 
 export default fetchAnime;
